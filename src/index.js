27a15aa82aedd25b119afd848bc64e752b2a0ebe
Sara Hentzel <sara_hentzel@sil.org>
2023-02-06 13:12:19 -0600

    TT-4743 split from processNewFile (trigger)

--- src/index.js
+++ src/index.js
@@ -1,107 +1,13 @@
-const https = require("https");
-const path = require("path");
-const mp3 = require("mp3-duration");
-const mm = require("music-metadata/lib/core");
-const { stringify } = require("querystring");
-const { format } = require("path");
+const AWS = require('aws-sdk');
 
-const host = process.env.SIL_TR_HOST;
-const stagepath = process.env.SIL_TR_URLPATH;
+const timeout = process.env.TIMEOUT;
+const queue = process.env.SIL_TR_EXPORT_QUEUE;
+const bucket = process.env.SIL_TR_USERFILES_BUCKET;
 
-exports.handler = async (event) => {
-  const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(
-    event.Records[0].s3.object.key.replace(/\+/g, " ")
-  );
-  console.log("hello! processing new s3 file!", key);
-  //key looks like 139647_Tes/126282_Luk/NIV11-LUK-001-001004v01.mp3
-  var parts = key.split("/");
-  var plan = parts[1].split("_")[0];
-
-  const filename = path.basename(key);
-  const filesize = event.Records[0].s3.object.size;
-
-  function getMedia() {
-    return new Promise((resolve, reject) => {
-      // options for API request
-      var options = {
-        host: host,
-        path: `${stagepath}/api/mediafiles/fromfile/${plan}/${encodeURI(
-          filename
-        )}`,
-        method: "GET",
-      };
-      const req = https.request(options, (res) => {
-        console.log("media statusCode:", res.statusCode, options);
-        if (res.statusCode === 404) {
-          //not found
-          reject(res.statusCode);
-        }
-        res.on("data", (d) => {
-          resolve(JSON.parse(d));
-        });
-      });
-
-      req.on("error", (e) => {
-        console.log("media error:", e);
-        reject(e);
-      });
-
-      // req.write(postData);
-
-      req.end();
-    });
-  }
-
-  async function patchMedia(id, filesize, duration) {
-    return new Promise((resolve, reject) => {
-      // options for API request
-      var options = {
-        host: host,
-        path:
-          stagepath +
-          "/api/mediafiles/" +
-          id +
-          "/fileinfo/" +
-          filesize +
-          "/" +
-          duration,
-        method: "PATCH",
-      };
-
-      const req = https.request(options, (res) => {
-        console.log("media statusCode:", res.statusCode, options);
-        if (res.statusCode === 404) {
-          //not found
-          reject(res.statusCode);
-        }
-        res.on("data", (d) => {
-          resolve(JSON.parse(d));
-        });
-      });
-
-      req.on("error", (e) => {
-        console.log("media error:", e);
-        reject(e);
-      });
-
-      // req.write(postData);
-
-      req.end();
-    });
-  }
-
-  async function getFile() {
-    const aws = require("aws-sdk");
-    const s3 = new aws.S3(); // Pass in opts to S3 if necessary
-    var params = {
-      Bucket: bucket, // your bucket name,
-      Key: key, // path to the object you're looking for
-    };
-    const data = await s3.getObject(params).promise();
-    return data.Body;
-  }
-
+exports.handler = async function(event, context) {
+  console.log('Here we are', event,event.Records.length);
+  var sqs = new AWS.SQS();   
+  
   async function getFileStream(filekey) {
     const aws = require("aws-sdk");
     const s3 = new aws.S3(); // Pass in opts to S3 if necessary
@@ -149,27 +55,31 @@ exports.handler = async (event) => {
 
   async function putFile(filekey, body) {
     const aws = require("aws-sdk");
-    const s3 = new aws.S3(); // Pass in opts to S3 if necessary
+    const s3 = new aws.S3(); 
+    console.log('putFile', filekey);
     var params = {
       Bucket: bucket,
       Key: filekey,
       Body: body,
-      ContentType: "application/ptf",
+      ContentType: "application/" +  filekey.substring(filekey.lastIndexOf(".")+1),
     };
+    console.log(params);
     var mu = s3.upload(params);
     var data = await mu.promise();
     console.log("file saved ", data.Location);
     return data.Location;
   }
 
-  async function exportProjectMedia() {
+  async function exportProjectMedia(info) {
     const AdmZip = require("adm-zip");
+    let start = info.Start;
+    const secondsOfWork = timeout-30;
+    const dtBail = Date.now() + secondsOfWork*1000;
+    let bailNow = false;
+    const inputKey = info.Folder + '/' + info.PTFFile; 
 
-    console.log("export offline project file", key);
-    var start = 0;
-    var zip = new AdmZip(await FileToBuffer(key));
-    var statusFile = key.substring(0, key.length - 3) + "sss";
-    await putFile(statusFile, "0");
+    var zip = new AdmZip(await FileToBuffer(inputKey));
+    var statusFile = inputKey + ".sss";
     var zipEntries = zip.getEntries();
     var deletemf = true;
     var mediafiles = zipEntries.find(
@@ -183,10 +93,11 @@ exports.handler = async (event) => {
     }
     if (mediafiles) {
       var mediastr = mediafiles.getData().toString("utf8");
-      if (deletemf) zip.deleteFile(mediafiles);
       var media = JSON.parse(mediastr);
       if (Array.isArray(media.data)) {
-        for (var element of media.data) {
+        for (let ix = start; !bailNow && ix < media.data.length; ix++) {
+          var element = media.data[ix];
+          console.log(ix.toString(), " of ", media.data.length.toString(), element.attributes["s3file"]);
           if (element.attributes["audio-url"]) {
             try {
               var buf = await FileToBuffer(element.attributes["s3file"]);
@@ -201,62 +112,86 @@ exports.handler = async (event) => {
             }
           }
           start += 1;
-          if (start % 10 === 0) {
-            await putFile(statusFile, start.toString());
+          bailNow = Date.now() > dtBail;
+          if (start % 20 === 0 || bailNow) {
+            await putFile(statusFile, start.toString() + " media");
+            if (bailNow)
+            {
+            var buf = zip.toBuffer();
+            await putFile(inputKey, buf);
+            }
           }
         }
-        console.log("done", start);
-        //backward compatible
-        var newKey = key.substring(0, key.length - 4);
-        if (!newKey.endsWith(".ptf") && !newKey.endsWith(".audio") && !newKey.endsWith(".burrito"))
-          newKey += ".ptf";
-
-        await putFile(newKey, zip.toBuffer());
-        await putFile(statusFile, "-1");
+        if (!bailNow) {
+          console.log("done", start);
+          if (deletemf) zip.deleteFile(mediafiles);
+          await putFile(inputKey, zip.toBuffer());
+          await putFile(statusFile, "-1");
+          start = -1;
+        }
       }
+    }
+    else {
+      await putFile(statusFile, "-1");
+      start = -1;
     }
     return start;
   }
-  try {
-    if (key.startsWith("exports")) {
-      if (key.endsWith("tmp")) return await exportProjectMedia();
-      return 0;
-    }
 
-    if (key.startsWith("imports")) return 0; // await importProject();
-
-    /* user media file */
-    var x = await getMedia();
-    var stream = null;
-    var tries = 0;
-    while (stream === null && tries < 5) {
-      console.log(tries);
-      stream = await getFileStream(key);
-      tries++;
-    }
-    if (stream !== null) {
-      var metadata = await mm.parseStream(
-        stream,
-        x.data.attributes["content-type"]
-      );
-      var duration = metadata.format.duration;
-
-      if (duration === undefined) {
-        duration = await mp3(await getFile()); //this doesn't do m4a files correctly so it's a backup only
-        console.log("Your file is " + duration + " seconds long - mm");
+  function sendMsg(params, successCallback, errorCallback) {
+    sqs.sendMessage(params, function(err, data) {
+      if (err) {
+        console.log(err);
+        errorCallback(err);
       } else {
-        console.log("Your file is " + duration + " seconds long - meta");
+        console.log(data.MessageId);
+        successCallback(data.MessageId);
       }
-      //patch it
-      var x = await patchMedia(x.data.id, filesize, duration);
-      return x;
-    } else {
-      console.log("file could not be opened");
-      return 0;
-    }
-  } catch (e) {
-    console.log("catch", key);
-    console.log(e);
-    return e;
+    });
   }
-};
+
+  async function sendMsgWrap(params)
+  {
+    return new Promise((resolve, reject) => {
+      sendMsg(params, (successResponse) => {resolve(successResponse)}, (errorResponse) => reject(errorResponse))
+    })
+  }
+  
+
+  try {
+    const { body ,attributes} =  event.Records[0];
+      var info = JSON.parse(body)
+      var start = await exportProjectMedia(info);
+      if (start > 0)
+      {
+        //send a message to start again
+        var params = {
+          MessageBody:JSON.stringify({...info, Start:start}),
+          MessageDeduplicationId: attributes.MessageGroupId + "_" + start.toString(),
+          MessageGroupId:attributes.MessageGroupId,
+          QueueUrl: queue
+        };
+        console.log(params);
+        const result = await sendMsgWrap(params);
+      }
+      /*
+        var deleteParams = {
+          QueueUrl: queue,
+          ReceiptHandle: msg.receiptHandle
+        };
+        console.log('deleteParams', deleteParams);
+        sqs.deleteMessage(deleteParams, function(err, data) {
+          if (err) {
+            console.log("Delete Error", err);
+          } else {
+            console.log("Message Deleted", data);
+          }
+        });
+        */
+      }
+      catch (err) {
+          console.log(err.toString());
+      }
+
+  
+}
